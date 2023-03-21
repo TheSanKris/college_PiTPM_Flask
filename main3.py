e84fedcf20e920be8b22f5b71deff996fd13c1ee
TheSanKris <Mironovshsa@mail.ru>
2023-03-21 16:26:03 +0300

    FTW2

---
+++ main3.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def requestdata():
+    return "Hello! Your IP is {} and you are using {}: ".format(request.remote_addr,  
+                                                                request.user_agent)
+
+if __name__ == "__main__":
+    app.run(debug=True)
